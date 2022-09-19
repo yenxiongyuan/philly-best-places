83b8d805a0821295a61f1a5b27654c128bcf2474
Yen Xiong Yuan <yenyuan17@gmail.com>
2022-09-19 20:26:04 +0000

    class 7

--- app.js
+++ app.js
@@ -1,18 +1,62 @@
-const userName = prompt ('What is your name?');
-console.log(`Hello, ${userName}`);
+// const userName = prompt ('What is your name?');
+// console.log(`Hello, ${userName}`);
 
-let gender = prompt ('Are you male or female');
+// let gender = prompt ('Are you male or female');
+// console.log(gender)
 
-message = (gender.toLowerCase == 'male') ? 'You are male.' : 'You are female.'
+// message = (gender.toLowerCase() === 'male') ? 'You are male.' : 'You are female.'
+// console.log(message)
 
-let userAge = prompt ('What is your age?');
 
-if (userAge < 21){
-message1 = 'You are too young to travel by yourself.'
-} else if (userAge >= 21 && userAge < 70){
-    message1 = 'You can travel by yourself.'
-} else {
-    message1 = 'Yor are too old to travel by yourself.'
+// let userAge = prompt ('What is your age?');
+
+// if (userAge < 21){
+// message1 = 'You are too young to travel by yourself.'
+// } else if (userAge >= 21 && userAge < 70){
+//     message1 = 'You can travel by yourself.'
+// } else {
+//     message1 = 'Yor are too old to travel by yourself.'
+// }
+
+// document.write(`Hello, ${userName}. ${message} Your age is ${userAge}. ${message1}`);
+
+
+function userName() {
+
+    let userName = prompt ('What is your name?');
+    return document.write(`Hello, ${userName}.`);
+};
+// userName();
+// console.log(userName());
+
+function userGender() {
+
+    let gender = prompt ('Are you male or female');
+    message = (gender.toLowerCase() === 'male') ? 'You are male.' : 'You are female.';
+    return document.write(message);
+};
+// userGender();
+// console.log(userGender());
+
+
+function userAge() {
+    let userAge = prompt ('What is your age?');
+    if (userAge < 21){
+    message1 = `You are ${userAge} years old. You are too young to travel by yourself.`;
+    } else if (userAge >= 21 && userAge < 70){
+    message1 = `You are ${userAge} years old. You can travel by yourself.`
+    } else {
+    message1 = `You are ${userAge} years old. You are too old to travel by yourself.`;
 }
+return document.write(message1);
+};
+// userAge();
+// console.log(userAge());
 
-document.write(`Hello, ${userName}. ${message} Your age is ${userAge}. ${message1}`);
+// funtion webMessage() {
+//     return `Hello, ${userName()}. ${userGender()} Your age is ${userAge()}. ${userAge()}`
+// };
+// webMessage();
+
+
+
