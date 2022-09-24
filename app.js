f616c76ab210568fbd0a32494f06e5792a53bb64
Yen Xiong Yuan <yenyuan17@gmail.com>
2022-09-24 03:27:28 +0000

    update class 7 and 8

--- app.js
+++ app.js
@@ -58,17 +58,72 @@ return document.write(message1);
 // };
 // webMessage();
 
-function showCityHall(){
-    const userNum = prompt('How many times do you want to see Philadelphia City Hall picture? Pick from 1- 5');
+// function showCityHall(){
+//     const userNum = prompt('How many times do you want to see Philadelphia City Hall picture? Pick from 1- 5');
     
-    for(let i = 0; (i < userNum && i < 5); i++ ){
+//     for(let i = 0; (i < userNum && i < 5); i++ ){
         
-        let pick = confirm("Press a button!\nEither OK or Cancel."); 
-        let docWriteP = document.write(`This is picture ${i + 1}`);
-        let docWrite = document.write("<img src='Philadelphia-City-Hall.jpg' style='width:250px;'/>")
-        choice = (pick === true) ? docWriteP + docWrite : alert('Please pick from 1- 5'); 
-        } 
+//         let pick = confirm("Press a button!\nEither OK or Cancel."); 
+//         let docWriteP = document.write(`This is picture ${i + 1}`);
+//         let docWrite = document.write("<img src='Philadelphia-City-Hall.jpg' style='width:250px;'/>")
+//         choice = (pick === true) ? docWriteP + docWrite : alert('Please pick from 1- 5'); 
+//         } 
+//     };
+
+
+    function philly() {
+
+        let phila = '';
+
+        while(phila != 12){
+            phila = prompt ("How many letters are in the word 'Philadelphia' Use number for the answer.");
+        
+            if(phila < 12){
+                 alert('Wrong answer. Your number is low. Please pick again.') 
+                } else if (phila > 12){
+                    alert ('Wrong answer. Your number is high. Please pick again.')
+                } else if (phila == 12){
+                    alert('You got the right answer') 
+                } else {
+                    alert('Please enter a number')
+                }
+            
+            }
+
     };
+
+
+    function showCityHall(){
+        
+        let userNum = '';
+        let fivetimepic = document.write("<img src='Philadelphia-City-Hall.jpg' style='width:250px;'/>");
+        let showtext = document.write('This is the  Philadelphia City Hall picture.')
+
+        while(userNum != 5){
+            userNum = prompt('Guess what is my favorite number? Hint: pick from 1- 5');
+
+            if(userNum < 5){
+                alert('Wrong number. Your number is low. Please pick again.')
+            } else if(userNum > 5){
+                alert('Wrong number. Your number is high. Please pick again.')
+            } else if(userNum == 5){
+                alert('You got the right number'),  showtext + fivetimepic 
+            } else {
+                alert('Please enter a number')
+            }
+           
+        }
+
+    };
+        
+        // for(let i = 0; (i < userNum && i < 5); i++ ){
+            
+        //     let pick = confirm("Press a button!\nEither OK or Cancel."); 
+        //     let docWriteP = document.write(`This is picture ${i + 1}`);
+        //     let docWrite = document.write("<img src='Philadelphia-City-Hall.jpg' style='width:250px;'/>")
+        //     choice = (pick === true) ? docWriteP + docWrite : alert('Please pick from 1- 5'); 
+        //     } 
+        // };
 
 
 
